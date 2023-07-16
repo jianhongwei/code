fb59873f68eff1ce07cb253cdd64e7ca38f6ea4f
jianhongwei <jianhongwei@kedacom.com>
2023-07-16 21:58:03 +0800

    增加offer_61解析代码

--- 01_sort/03_offer_61.cpp
+++ 01_sort/03_offer_61.cpp
@@ -54,6 +54,39 @@ inline void bubble_sort_1(int *data, const int len)
     }
 }
 
+bool is_continues(int *data, const int len)
+{
+    int zero_cnt = 0;
+    int sum_diff = 0;
+    int sum_ele[13] = {0};
+    for (size_t i = 0; i < len; i++)
+    {
+        if (data[i] == 0)
+        {
+            zero_cnt++;
+        }
+        else
+        {
+            sum_ele[data[i]]++;
+            if (sum_ele[data[i]] > 1)
+            {
+                return false;
+            }
+            if (i < len - 1)
+            {
+                sum_diff += data[i + 1] - data[i];
+            }
+        }
+    }
+
+    if (sum_diff <= 4)
+    {
+        return true;
+    }
+    return false;
+}
+
+// 从若干副扑克牌中随机抽 5 张牌，判断是不是一个顺子，即这5张牌是不是连续的。2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0 ，可以看成任意数字。A 不能视为 14。
 int main()
 {
     const int LEN = 5;
@@ -73,6 +106,17 @@ int main()
     {
         std::cout << data[i] << " ";
     }
+
+    bool bContinues = is_continues(data, LEN);
+    if (bContinues)
+    {
+        std::cout << "是连续的" << std::endl;
+    }
+    else
+    {
+        std::cout << "不是连续的" << std::endl;
+    }
+
     std::cout << std::endl;
     return 0;
 }
